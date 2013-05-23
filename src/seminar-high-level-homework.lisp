1c93e4f70c5cc20407947353dc129b452e885ef1
Jan Winkler <winkler@cs.uni-bremen.de>
2013-05-23 17:29:04 +0200

    Removed those awkward BSD header notes (not necessary for seminar templates) and added basic functionality

--- src/seminar-high-level-homework.lisp
+++ src/seminar-high-level-homework.lisp
@@ -1,28 +1,42 @@
-;;; Copyright (c) 2013, Jan Winkler <winkler@cs.uni-bremen.de>
-;;; All rights reserved.
-;;; 
-;;; Redistribution and use in source and binary forms, with or without
-;;; modification, are permitted provided that the following conditions are met:
-;;; 
-;;;     * Redistributions of source code must retain the above copyright
-;;;       notice, this list of conditions and the following disclaimer.
-;;;     * Redistributions in binary form must reproduce the above copyright
-;;;       notice, this list of conditions and the following disclaimer in the
-;;;       documentation and/or other materials provided with the distribution.
-;;;     * Neither the name of Universität Bremen nor the names of its
-;;;       contributors may be used to endorse or promote products derived from
-;;;       this software without specific prior written permission.
-;;; 
-;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-;;; POSSIBILITY OF SUCH DAMAGE.
-
 (in-package :seminar-high-level-homework)
+
+;; The *cities* variable holds all cities that are available. They are
+;; given as a list which consists of lists of key-value pairs. In our
+;; case, the keys `name' and `coordinates' are defined.
+(defvar *cities* (list (list (cons 'name "City 1")
+                             (cons 'coordinates '(1.0 3.0)))
+                       (list (cons 'name "City 2")
+                             (cons 'coordinates '(3.0 1.0)))
+                       (list (cons 'name "City 3")
+                             (cons 'coordinates '(-1.0 0.0)))
+                       (list (cons 'name "City 4")
+                             (cons 'coordinates '(2.0 2.0)))
+                       (list (cons 'name "City 5")
+                             (cons 'coordinates '(5.0 0.0)))
+                       (list (cons 'name "City 6")
+                             (cons 'coordinates '(-4.0 0.0)))
+                       (list (cons 'name "City 7")
+                             (cons 'coordinates '(1.0 1.0)))
+                       (list (cons 'name "City 8")
+                             (cons 'coordinates '(1.0 1.0)))
+                       (list (cons 'name "City 9")
+                             (cons 'coordinates '(1.0 1.0)))
+                       (list (cons 'name "City 10")
+                             (cons 'coordinates '(1.0 1.0)))))
+
+(defun read-name (city)
+  (cdr (assoc 'name city)))
+
+;; TASK: Write a function for reading the coordinates of a given
+;; city. The function `read-name' shows how to read the respective
+;; name from a given city dataset.
+
+(defun read-coordinates (city)
+  (declare (ignore city)))
+
+(defun send-turtle-velocity (linear angular)
+  (roslisp:publish (roslisp:advertise "/turtle1/command_velocity"
+                                      "turtlesim/Velocity")
+                   (roslisp:make-message "turtlesim/Velocity"
+                                         linear linear
+                                         angular angular)))
